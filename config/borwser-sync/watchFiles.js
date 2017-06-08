25a1eb8fdfa50bd304a2c1e4d8db6201cf76e99f
liuzihao <951919064@qq.com>
2017-06-09 07:28:29 +0800

    live

--- config/borwser-sync/watchFiles.js
+++ config/borwser-sync/watchFiles.js
@@ -2,7 +2,7 @@ var path =require('path');
 var rootPath = require('../helper').rootPath;
 
 // 需要监听的目录
-var watchDir = path.resolve(rootPath+'/src/');
+var watchDir = path.resolve('/work/web/kaoyayacn');
 // 监听文件的基本路径
 
 var basePath = path.relative(rootPath,watchDir);
@@ -10,10 +10,11 @@ if(basePath!=''){
   basePath+="/"
 }
 var watachFiles = [
-  "**/*.html",
-  "**/*.css",
-  "**/*.js"
+  "knew/View/**/*.html",
+  "source/css/**/*.css",
+  "source/js/**/*.js"
 ].map(function(val){
   return basePath+ val;
 })
+console.log(watachFiles)
 module.exports = watachFiles;
