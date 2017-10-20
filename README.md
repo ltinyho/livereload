35bb77a3b442d69a545cca3c68a3f8e5fc1f536a
ltinyho <ltinyho@gmail.com>
2017-10-20 17:01:42 +0800

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ### gulp+browser-sync 实现实时监听文件修改
 
- - 设置监听文件路径：config/browser-sync/watchFiles.js
+先全局安装 gulp
+```bash
+cnpm install gulp -g
 
-```
-npm run dev
+gulp  # watchkaoyayacn
+gulp accgg # watch accgg  
+
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp')
 var browserSync = require("browser-sync").create();
 var generateConfig = require('./config').generateConfig;
 
+gulp.task("accgg",function () {
+  browserSync.init(generateConfig('accgg'),function () {
+  })
+})
 
 gulp.task("default",function () {
   browserSync.init(generateConfig('kaoyayacn'),function () {
-  })
-})
-gulp.task("accgg",function () {
-  browserSync.init(generateConfig('accgg'),function () {
   })
 })
